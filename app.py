f8f5f59f9024148b0ac1f349785ade24594d9f2d
itamardavideliyahu-beep <itamardavideliyahu@gmail.com>
2025-10-26 22:43:39 +0200

    second update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from my first CI/CD pipeline! first update"
+    return "Hello from my first CI/CD pipeline! first update second update"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
